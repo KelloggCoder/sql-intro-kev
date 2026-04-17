266a0677606835a90861082ce8d8616486cf6aa4
KelloggCoder <keval.sheth@kellogg.northwestern.edu>
2026-04-17 04:00:10 +0000

    best home run hitter by team

--- lab-4-4.sql
+++ lab-4-4.sql
@@ -38,3 +38,11 @@
 -- +-------------------------------+------------+-------------+----------------------+
 
 
+SELECT teams.name, players.first_name, players.last_name, max(stats.home_runs)
+FROM players
+INNER JOIN stats 
+    ON stats.player_id = players.id 
+INNER JOIN teams
+    ON teams.id = stats.team_id
+WHERE teams.year = 2019
+GROUP BY teams.name;
